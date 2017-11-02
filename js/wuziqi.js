f9fab6c1215e113cdb5280a4b31532e275b6e51b
Your Name <you@example.com>
2017-11-02 13:42:26 +0800

    1

--- js/wuziqi.js
+++ js/wuziqi.js
@@ -1,257 +1,302 @@
-let kaishi = document.querySelector(".kaishi");
-let neirong = document.querySelector(".neirong");
-let canvas = document.querySelector("canvas");
-let AI = document.querySelector(".moshi1");
-let wanjia = document.querySelector(".moshi");
-let kongzhi = document.querySelector(".kongzhi")
-let youximoshi = document.querySelector(".youximoshi");
-let mask = document.querySelector(".mask");
-let h = document.querySelector(".mask h1");
-let set = document.querySelector(".set");
-let isAI = false;
-let flag = true;
-let pos = {};
-let blank = {};
-let output = document.querySelector(".qipu");
-let imgbox = document.querySelector(".imgbox");
-let xiazai = document.querySelector(".mask a");
-let flag3 = true;
-let w = 40;
-let huabi = canvas.getContext("2d");
-let music = document.querySelector(".yinyue");
-let audio = document.querySelector("audio");
-let set1 = document.querySelector(".set1");
-let flag2 = true;
-//游戏开始点击事件
-kaishi.onclick = function () {
-    neirong.classList.add("show")
-    kaishi.style.display = "none";
-    kongzhi.style.display = "block";
-    youximoshi.classList.add("moshi3");
-    wanjia.classList.add("moshi2");
-};
-//生成棋盘函数
-function qipan() {
-    huabi.beginPath();
-    for (let a = 1; a < 14; a++) {
-        huabi.save()
-        huabi.lineWidth = 1;
-        huabi.moveTo(20.5, a * w + 20.5);
-        huabi.lineTo(580, a * w + 20.5);
-        huabi.moveTo(a * w + 20.5, 20.5);
-        huabi.lineTo(a * w + 20.5, 580);
-        huabi.stroke();
-        huabi.restore();
-        huabi.rect(20.5, 20.5, 560, 560);
-        for (let i = 0; i < 15; i++) {
-            for (let b = 0; b < 15; b++) {
-                blank[j(i, b)] = true;
+class Game {
+    constructor() {
+        this.kaishi = document.querySelector(".kaishi");//游戏开始按钮
+        this.wanjia = document.querySelector(".moshi");//玩家模式按钮
+        this.AI = document.querySelector(".moshi1");//人机模式按钮
+        this.set1 = document.querySelector(".set1");//音乐开关按钮
+        this.set = document.querySelector(".set");//重新开始按钮
+        this.output = document.querySelector(".qipu");//生成棋谱按钮
+        this.xiazai = document.querySelector(".mask a");//下载棋谱按钮
+        this.neirong = document.querySelector(".neirong"); //画布盒子
+        this.canvas = document.querySelector(".canvass");    //画布
+        this.kongzhi = document.querySelector(".kongzhi"); //控制模式盒子
+        this.youximoshi = document.querySelector(".youximoshi");//游戏模式字
+        this.mask = document.querySelector(".mask");//游戏结束盒子
+        this.imgbox = document.querySelector(".imgbox");//生成棋谱盒子
+        this.huabi = this.canvas.getContext("2d");//声明画笔属性
+        this.music = document.querySelector(".yinyue");//音乐图标盒子
+        this.audio = document.querySelector("audio");//音乐
+        this.h = document.querySelector("h1");//获胜结果盒子
+        this.flag = true;
+        this.flag2 = true;//音乐开关
+        this.flag3 = true;//生成棋谱开关
+        this.w = 40; //每个棋盘格子宽度
+        this.pos = {}; //保存对象
+        this.blank = {};//保存人机对象属性
+        this.isAI = false; //人机默认关
+        this.anniu = document.querySelector(".mask a input")
+    }
+
+    //棋盘划线方法
+    _qipan() {
+        this.huabi.beginPath();
+        for (let a = 1; a < 14; a++) {
+            this.huabi.save()
+            this.huabi.lineWidth = 1;
+            this.huabi.moveTo(20.5, a * this.w + 20.5);
+            this.huabi.lineTo(580, a * this.w + 20.5);
+            this.huabi.moveTo(a * this.w + 20.5, 20.5);
+            this.huabi.lineTo(a * this.w + 20.5, 580);
+            this.huabi.stroke();
+            // console.log(this.huabi)
+            this.huabi.restore();
+            this.huabi.rect(20.5, 20.5, 560, 560);
+            for (let i = 0; i < 15; i++) {
+                for (let b = 0; b < 15; b++) {
+                    this.blank[this._j(i, b)] = true;
+                }
+            }
+
+        }
+        this._dian(3, 3);
+        this._dian(7, 7);
+        this._dian(3, 11);
+        this._dian(11, 3);
+        this._dian(11, 11);
+    }
+
+    //棋盘画五点方法
+        _dian(x, y) {
+            console.log(this.huabi)
+            this.huabi.save();
+            this.huabi.beginPath();
+            this.huabi.translate(x * this.w, y * this.w)
+            this.huabi.arc(20.5, 20.5, 6, 0, 2 * Math.PI);
+            this.huabi.fill();
+            this.huabi.restore();
+        };
+
+    //生成棋子方法
+    _qizi(x, y, color) {
+        this.huabi.save();
+        this.huabi.beginPath();
+        this.huabi.fillStyle = color;
+        this.huabi.arc(x * this.w + 20, y * this.w + 20, 15, 0, 2 * Math.PI);
+        this.huabi.fill();
+        this.huabi.restore();
+        this.pos[this._j(x, y)] = color;
+        delete this.blank[this._j(x, y)];
+    }
+
+    //辅助J方法
+    _j(x, y) {
+        return x + "_" + y;
+    };
+
+    //游戏规则
+    _guize(x, y, color) {
+        let row = 1;
+        let col = 1;
+        let row1 = 1;
+        let col1 = 1;
+        let i = 1;
+        while (this.pos[this._j(x + i, y)] === color) {
+            row++;
+            i++;
+        }
+        i = 1;
+        while (this.pos[this._j(x - i, y)] === color) {
+            row++;
+            i++;
+        }
+        i = 1;
+        while (this.pos[this._j(x, y + i)] === color) {
+            col++;
+            i++;
+        }
+        i = 1;
+        while (this.pos[this._j(x, y - i)] === color) {
+            col++;
+            i++;
+        }
+        i = 1;
+        while (this.pos[this._j(x + i, y - i)] === color) {
+            row1++;
+            i++;
+        }
+        i = 1;
+
+        while (this.pos[this._j(x - i, y + i)] === color) {
+            row1++;
+            i++;
+        }
+        i = 1;
+        while (this.pos[this._j(x + i, y + i)] === color) {
+            col1++;
+            i++;
+        }
+        i = 1;
+        while (this.pos[this._j(x - i, y - i)] === color) {
+            col1++;
+            i++;
+        }
+        return Math.max(row, row1, col, col1);
+    }
+
+    //AI智能设置
+    _getpos() {
+        let max = 0;
+        let pos1 = {};
+        for (let i in this.blank) {
+            let x = parseInt(i.split("_")[0]);
+            let y = parseInt(i.split("_")[1]);
+            let changdu = this._guize(x, y, "#fff");
+            if (changdu > max) {
+                max = changdu;
+                pos1 = {x, y}
             }
         }
-
-    }
-    dian(3, 3);
-    dian(7, 7);
-    dian(3, 11);
-    dian(11, 3);
-    dian(11, 11);
-
-    function dian(x, y) {
-        huabi.save();
-        huabi.beginPath();
-        huabi.translate(x * w, y * w)
-        huabi.arc(20.5, 20.5, 6, 0, 2 * Math.PI);
-        huabi.fill();
-        huabi.restore();
-    }
-}
-qipan();
-//生成棋子函数
-function qizi(x, y, color) {
-    huabi.save();
-    huabi.beginPath();
-    huabi.fillStyle = color;
-    huabi.arc(x * w + 20, y * w + 20, 15, 0, 2 * Math.PI);
-    huabi.fill();
-    huabi.restore();
-    pos[j(x, y)] = color;
-    delete blank[j(x, y)];
-
-
-}
-//生成棋子事件
-canvas.onclick = function (e) {
-    let x = Math.round((e.offsetX - 20) / w);
-    let y = Math.round((e.offsetY - 20) / w);
-    if (pos[j(x, y)]) {
-        return
-    }
-    if (flag) {
-        qizi(x, y, "#000");
-        if (guize(x, y, "#000") === 5) {
-            over("黑")
-
-
-        }
-        if (isAI) {
-            let p = getpos();
-            qizi(p.x, p.y, "#fff");
-            if (guize(p.x, p.y, "#fff") === 5) {
-                over("白")
+        let max2 = 0
+        let pos2 = {};
+        for (let i in this.blank) {
+            let x = parseInt(i.split("_")[0]);
+            let y = parseInt(i.split("_")[1]);
+            let changdu = this._guize(x, y, "#000");
+            if (changdu > max2) {
+                max2 = changdu;
+                pos2 = {x, y}
             }
-            return;
         }
-
-    } else {
-        qizi(x, y, "#fff");
-        if (guize(x, y, "#fff") === 5) {
-            over("白")
-        }
-
-
-    }
-    flag = !flag;
-
-};
-//辅助j属性
-function j(x, y) {
-    return x + "_" + y;
-}
-//游戏规则
-function guize(x, y, color) {
-    let row = 1;
-    let col = 1;
-    let row1 = 1;
-    let col1 = 1;
-    let i = 1;
-    while (pos[j(x + i, y)] === color) {
-        row++;
-        i++;
-    }
-    i = 1;
-    while (pos[j(x - i, y)] === color) {
-        row++;
-        i++;
-    }
-    i = 1;
-    while (pos[j(x, y + i)] === color) {
-        col++;
-        i++;
-    }
-    i = 1;
-    while (pos[j(x, y - i)] === color) {
-        col++;
-        i++;
-    }
-    i = 1;
-    while (pos[j(x + i, y - i)] === color) {
-        row1++;
-        i++;
-    }
-    i = 1;
-
-    while (pos[j(x - i, y + i)] === color) {
-        row1++;
-        i++;
-    }
-    i = 1;
-    while (pos[j(x + i, y + i)] === color) {
-        col1++;
-        i++;
-    }
-    i = 1;
-    while (pos[j(x - i, y - i)] === color) {
-        col1++;
-        i++;
-    }
-    return Math.max(row, row1, col, col1);
-}
-//玩家模式点击事件
-wanjia.onclick = function () {
-    isAI = false;
-    clear();
-    neirong.classList.add("show")
-    wanjia.classList.add("moshi2");
-    AI.classList.remove("moshi2");
-}
-//AI 模式点击事件
-AI.onclick = function () {
-    isAI = true;
-    clear();
-    neirong.classList.add("show")
-    AI.classList.add("moshi2");
-    wanjia.classList.remove("moshi2");
-}
-//AI智能设置
-function getpos() {
-    let max = 0;
-    let pos1 = {};
-    for (let i in blank) {
-        let x = parseInt(i.split("_")[0]);
-        let y = parseInt(i.split("_")[1]);
-        let changdu = guize(x, y, "#fff");
-        if (changdu > max) {
-            max = changdu;
-            pos1 = {x, y}
+        if (max > max2) {
+            return pos1;
+        } else {
+            return pos2;
         }
     }
-    let max2 = 0
-    let pos2 = {};
-    for (let i in blank) {
-        let x = parseInt(i.split("_")[0]);
-        let y = parseInt(i.split("_")[1]);
-        let changdu = guize(x, y, "#000");
-        if (changdu > max2) {
-            max2 = changdu;
-            pos2 = {x, y}
-        }
+
+    //游戏结束方法
+    _over(name) {
+        this.mask.style.display = "block";
+        this.h.innerHTML = name + "棋获胜"
     }
-    if (max > max2) {
-        return pos1;
-    } else {
-        return pos2;
+
+    //恢复默认方法
+    _clear() {
+        this.mask.style.display = "none";
+        this.neirong.classList.remove("show");
+        this.huabi.clearRect(0, 0, 640, 640)
+        this.pos = {};
+        this._qipan();
+        // this._wudian()
+    }
+
+    //游戏开始方法
+    start() {
+        this.kaishi.addEventListener("click", function () {
+            this.neirong.classList.add("show");
+            this.kaishi.style.display = "none";
+            this.kongzhi.style.display = "block";
+            this.youximoshi.classList.add("moshi3");
+            this.wanjia.classList.add("moshi2");
+            this._qipan();
+            this._wudian();
+        }.bind(this))
+
+    }
+
+
+    //生成棋子事件
+    xiaqi() {
+        this.canvas.onclick = function (e) {
+            let x = Math.round((e.offsetX - 20) / this.w);
+            let y = Math.round((e.offsetY - 20) / this.w);
+            if (this.pos[this._j(x, y)]) {
+                return
+            }
+            if (this.flag) {
+                this._qizi(x, y, "#000");
+                if (this._guize(x, y, "#000") === 5) {
+                    this._over("黑")
+                }
+                if (this.isAI) {
+                    let p = this._getpos();
+                    this._qizi(p.x, p.y, "#fff");
+                    if (this._guize(p.x, p.y, "#fff") === 5) {
+                        this._over("白")
+                    }
+                    return;
+                }
+            } else {
+                this._qizi(x, y, "#fff");
+                if (this._guize(x, y, "#fff") === 5) {
+                    this._over("白")
+                }
+            }
+            this.flag = !this.flag;
+
+        }.bind(this);
+    }
+
+    //玩家模式点击事件
+    wanjia1() {
+        this.wanjia.addEventListener("click", function () {
+            this.isAI = false;
+            this._clear();
+            this.neirong.classList.add("show")
+            this.wanjia.classList.add("moshi2");
+            this.AI.classList.remove("moshi2");
+        }.bind(this))
+    }
+
+    //AI 模式点击事件
+    ai() {
+        this.AI.addEventListener("click", function () {
+            this.isAI = true;
+            this._clear();
+            this.neirong.classList.add("show")
+            this.AI.classList.add("moshi2");
+            this.wanjia.classList.remove("moshi2");
+        }.bind(this));
+    }
+
+    //重新开始点击事件
+    chongxin() {
+        this.set.addEventListener("click", function () {
+            this._clear()
+        }.bind(this));
+    }
+
+
+    //音乐点击事件
+    music1() {
+        this.set1.addEventListener("click", function () {
+            if (this.flag2) {
+                this.music.style.animationPlayState = "paused";
+                this.audio.pause();
+                this.set1.innerHTML = "音乐：开"
+            } else {
+                this.music.style.animationPlayState = "running";
+                this.audio.play();
+                this.set1.innerHTML = "音乐：关"
+            }
+            this.flag2 = !this.flag2;
+        }.bind(this))
+
+    }
+
+    //下载棋谱点击事件
+    xiazai1() {
+        this.output.addEventListener("click", function () {
+            if (this.flag3) {
+                this.imgbox.style.display = "block";
+                this.anniu.style.display="block";
+                let url = this.canvas.toDataURL();
+                let newimg = new Image();
+                newimg.src = url;
+                this.imgbox.appendChild(newimg);
+                this.xiazai.href = url;
+                this.xiazai.setAttribute("download", "棋谱.png")
+            }
+            this.flag3 = false;
+        }.bind(this))
     }
 }
-//游戏结束函数
-function over(name) {
-    mask.style.display = "block";
-    h.innerHTML = "<h1>" + name + "棋获胜</h1>"
-}
-//游戏结束事件
-set.onclick = function () {
-    clear()
-};
-//恢复默认函数
-function clear() {
-    mask.style.display = "none";
-    neirong.classList.remove("show");
-    huabi.clearRect(0, 0, 640, 640)
-    pos = {};
-    qipan();
-}
-//音乐点击事件
-set1.onclick = function () {
-    if (flag2) {
-        music.style.animationPlayState = "paused";
-        audio.pause();
-        set1.innerHTML = "音乐：开"
-    } else {
-        music.style.animationPlayState = "running";
-        audio.play();
-        set1.innerHTML = "音乐：关"
-    }
-    flag2 = !flag2;
-}
-//下载棋谱点击事件
-output.onclick = function () {
-    if (flag3) {
-        imgbox.style.display = "block";
-        let url = canvas.toDataURL();
-        let newimg = new Image();
-        newimg.src = url;
-        imgbox.appendChild(newimg);
-        xiazai.href = url;
-        xiazai.setAttribute("download", "棋谱.png")
-    }
-    flag3 = false;
-}
+
+let wuziqi1 = new Game();
+wuziqi1.start();
+wuziqi1.xiaqi();
+wuziqi1.wanjia1();
+wuziqi1.ai();
+wuziqi1.chongxin();
+wuziqi1.music1();
+wuziqi1.xiazai1();
